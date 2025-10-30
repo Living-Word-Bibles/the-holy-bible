06b2d326c009cd6fbb1e5757782ef7040f070fcb
Living-Word-Bibles <gospel@livingwordbibles.com>
2025-10-30 08:03:29 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,3 +6,4 @@ self.addEventListener("install", e=>{
 self.addEventListener("fetch", e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
+<script>if('serviceWorker' in navigator){ navigator.serviceWorker.register('/sw.js'); }</script>
